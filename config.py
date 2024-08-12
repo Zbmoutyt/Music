a9f7d2d9f3c1bfde63cb48a27ff4b9d4af478357
Zbmoutyt <167689143+Zbmoutyt@users.noreply.github.com>
2024-08-12 16:56:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
    "UPSTREAM_REPO",
-   "https://github.com/rudra0055/Music",
+   "https://github.com/Zbmoutyt/Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Rudra")
 GIT_TOKEN = getenv(
