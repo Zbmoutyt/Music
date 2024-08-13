b84500b1acf92d4f2ba7ee6a3f2578622c2f1277
Zbmoutyt <167689143+Zbmoutyt@users.noreply.github.com>
2024-08-13 19:44:59 +0530

    Update owner.py

--- Rudra/plugins/tools/owner.py
+++ Rudra/plugins/tools/owner.py
@@ -15,7 +15,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🗡️ 𝐎ᴡɴᴇʀ 🗡️", url=f"https://t.me/ll_HARD_TOO_CATCH_ll"
+                        "🗡️ 𝐎ᴡɴᴇʀ 🗡️", url=f"https://t.me/RAVIKR6206"
                     )],
             ]
         ),
@@ -31,7 +31,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🗡️ 𝐎ᴡɴᴇʀ 🗡️", url=f"https://t.me/ll_HARD_TOO_CATCH_ll"
+                        "🗡️ 𝐎ᴡɴᴇʀ 🗡️", url=f"https://t.me/RAVIKR6206"
                     )],
             ]
         ),
@@ -47,7 +47,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🗡️ 𝐎ᴡɴᴇʀ 🗡️", url=f"https://t.me/ll_HARD_TOO_CATCH_ll"
+                        "🗡️ 𝐎ᴡɴᴇʀ 🗡️", url=f"https://t.me/RAVIKR6206"
                     )],
             ]
         ),
