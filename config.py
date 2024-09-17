ea0d3f21a7c65759ab674cc05a0dcecb592ed7f1
Zbmoutyt <167689143+Zbmoutyt@users.noreply.github.com>
2024-09-17 19:04:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # Add Owner Username without @ 
-OWNER_USERNAME = getenv("OWNER_USERNAME","CHADDI_CHOR")
+OWNER_USERNAME = getenv("OWNER_USERNAME","Surya_Putra_Karn")
 # Get Your bot username
 BOT_USERNAME = getenv("BOT_USERNAME" , "Sizhoo_Music_Bot")
 # Don't Add style font 
