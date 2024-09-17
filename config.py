47fa63c1bb211a7cadd9642776f5a2f3af4d9350
Zbmoutyt <167689143+Zbmoutyt@users.noreply.github.com>
2024-09-17 20:21:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ GIT_TOKEN = getenv(
    "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", https://t.me/angrybird_worldx)
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/angrybird_worldx")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/angrybird_worldx")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
